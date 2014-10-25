fdfb0e91313002ac37223e09e8768ac5f41528c6
Korey Gall <koreygall@gmail.com>
2014-10-25 16:20:06 -0400

    First attempt to add celebration functionality to GitTest.java

--- GitTest.java
+++ GitTest.java
@@ -1,7 +1,14 @@
 public class GitTest {
-	
-	public static void main(String[] args) {
-	
-	
-	}
+    
+    public static void celebrate(boolean celebrate) {
+    
+        if (celebrate == true) System.out.println("Celebrate!!!");
+        else System.out.println("No celebration for you.");
+    
+    }
+    
+    public static void main(String[] args) {
+        celebrate(false);
+        
+    }
 }
